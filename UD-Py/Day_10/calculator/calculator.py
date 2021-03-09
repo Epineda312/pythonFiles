98d836e5a3f05699997a9e0648deea920d216c2c
Airic <epineda773@protonmail.com>
2021-03-09 02:19:15 -0600

    Add files via upload

---
+++ UD-Py/Day_10/calculator/calculator.py
@@ -0,0 +1,51 @@
+from art import logo
+#Calculator
+
+#Add 
+def add(n1, n2):
+  return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+  return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+#Divide
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+}
+
+# more_numbers = True
+# while more_numbers:
+def calculator():
+  print(logo)
+  
+  num1 = int(input("What's the first number?: "))
+  for symbol in operations:
+    print (symbol)
+  should_continue = True
+
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = int(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if  input(f"type 'y' to continue calculating wih {answer}, or type 'n' to start a new.: ").lower() == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
