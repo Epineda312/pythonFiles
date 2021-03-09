5b5bb0410269f19c17bb828bf31a5ceee36b8c51
Airic <epineda773@protonmail.com>
2021-03-09 02:21:27 -0600

    Update calculator.py

--- UD-Py/Day_10/calculator/calculator.py
+++ UD-Py/Day_10/calculator/calculator.py
@@ -29,14 +29,14 @@ operations = {
 def calculator():
   print(logo)
   
-  num1 = int(input("What's the first number?: "))
+  num1 = float(input("What's the first number?: "))
   for symbol in operations:
     print (symbol)
   should_continue = True
 
   while should_continue:
     operation_symbol = input("Pick an operation: ")
-    num2 = int(input("What's the next number?: "))
+    num2 = float(input("What's the next number?: "))
     calculation_function = operations[operation_symbol]
     answer = calculation_function(num1, num2)
 
