b2f1bfd2ab26e62a816dceefef762ef26613fa13
Airic <epineda773@protonmail.com>
2021-03-10 03:08:51 -0600

    Update blackjack.py

--- UD-Py/Day_11/blackjack/blackjack.py
+++ UD-Py/Day_11/blackjack/blackjack.py
@@ -26,22 +26,22 @@ def compare(user_score, computer_score):
 
     # Bug fix. If you and the computer are both over, you lose.
     if user_score > 21 and computer_score > 21:
-        return "You went over. You lose 😤"
+        return "You went over. You lose"
 
     if user_score == computer_score:
-        return "Draw 🙃"
+        return "Draw"
     elif computer_score == 0:
-        return "Lose, opponent has Blackjack 😱"
+        return "Lose, opponent has Blackjack"
     elif user_score == 0:
-        return "Win with a Blackjack 😎"
+        return "Win with a Blackjack"
     elif user_score > 21:
-        return "You went over. You lose 😭"
+        return "You went over. You lose"
     elif computer_score > 21:
-        return "Opponent went over. You win 😁"
+        return "Opponent went over. You win"
     elif user_score > computer_score:
-        return "You win 😃"
+        return "You win"
     else:
-        return "You lose 😤"
+        return "You lose"
 
 def play_game():
     print(logo)
