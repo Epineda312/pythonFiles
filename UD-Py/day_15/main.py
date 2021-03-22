6326df36b960f4b07ae04ce5954281edfbe7f44e
Airic <epineda773@protonmail.com>
2021-03-22 16:29:19 -0500

    Add files via upload

--- UD-Py/day_15/main.py
+++ UD-Py/day_15/main.py
@@ -1,7 +1,7 @@
 from menu import MENU, resources
 
 
-def report(supply):
+def starting_supply(supply):
     machine_water = supply["water"]
     machine_milk = supply["milk"]
     machine_coffee = supply["coffee"]
@@ -11,27 +11,27 @@ def report(supply):
 
 
 machine_is_on = True
+resources["money"] = 0
 water = resources["water"]
 milk = resources["milk"]
 coffee = resources["coffee"]
 espresso_price = MENU["espresso"]["cost"]
 latte_price = MENU["latte"]["cost"]
 cappuccino_price = MENU["cappuccino"]["cost"]
-resources["money"] = 0
+print(f"Starting resources are as follows:\n{starting_supply(resources)}")
 
 while machine_is_on:
-    print(f"Starting resources are as follows:\n{report(resources)}")
-
     order_complete = False
     while not order_complete:
         choice = input("What would you like? (espresso/latte/cappuccino)\n").lower()
-        print(f"One {choice} coming right up!")
         if choice == "espresso":
-            print(f"Making {choice}")
+            print(f"One {choice} coming right up!\n")
             if water < MENU["espresso"]["ingredients"]["water"]:
                 print("Not enough water")
+                break
             if coffee < MENU["espresso"]["ingredients"]["coffee"]:
                 print("Not enough coffee")
+                break
             else:
                 print("insert coins please")
                 quarters = float(input("How many quarters?: "))
@@ -39,27 +39,28 @@ while machine_is_on:
                 nickels = float(input("How many nickles?: "))
                 pennies = float(input("How many pennies?: "))
                 total = round((0.25 * quarters) + (0.10 * dimes) + (0.05 * nickels) + (0.01 * pennies), 2)
-                print(f"total coins inserted is ${total}")
+                print(f"total coins inserted is ${total}\n")
                 if total >= espresso_price:
                     change = round(total - espresso_price, 2)
                     print(f"Here is ${change} in change.")
                     print(f"Here is your {choice}. enjoy!")
                     resources["money"] = resources["money"] + espresso_price
-                    print(f"Current money in the machine = ${resources['money']}")
                     water = water - MENU["espresso"]["ingredients"]["water"]
                     coffee = coffee - MENU["espresso"]["ingredients"]["coffee"]
-                    print(f"After this drink, machine holds: {water} water and: {coffee} coffee")
                 elif total < espresso_price:
                     print("Sorry that's not enough money. Money refunded.")
 
         elif choice == "latte":
-            print(f"Making {choice}")
+            print(f"One {choice} coming right up!\n")
             if water < MENU["latte"]["ingredients"]["water"]:
                 print("Not enough water")
+                break
             if milk < MENU["latte"]["ingredients"]["milk"]:
                 print("Not enough milk")
-            if coffee < MENU["latter"]["ingredients"]["coffee"]:
+                break
+            if coffee < MENU["latte"]["ingredients"]["coffee"]:
                 print("Not enough coffee")
+                break
             else:
                 print("insert coins please")
                 quarters = float(input("How many quarters?: "))
@@ -73,21 +74,22 @@ while machine_is_on:
                     print(f"Here is ${change} in change.")
                     print(f"Here is your {choice}. enjoy!")
                     resources["money"] = resources["money"] + latte_price
-                    print(f"Current money in the machine = ${resources['money']}")
                     water = water - MENU["latte"]["ingredients"]["water"]
                     coffee = coffee - MENU["latte"]["ingredients"]["coffee"]
-                    print(f"After this drink, machine holds: {water} water and: {coffee} coffee")
                 elif total < latte_price:
                     print("Sorry that's not enough money. Money refunded.")
 
         elif choice == "cappuccino":
-            print(f"Making {choice}")
+            print(f"One {choice} coming right up!\n")
             if water < MENU["cappuccino"]["ingredients"]["water"]:
                 print("Not enough water")
+                break
             if milk < MENU["cappuccino"]["ingredients"]["milk"]:
                 print("Not enough milk")
+                break
             if coffee < MENU["cappuccino"]["ingredients"]["coffee"]:
                 print("Not enough coffee")
+                break
             else:
                 print("insert coins please")
                 quarters = float(input("How many quarters?: "))
@@ -95,21 +97,23 @@ while machine_is_on:
                 nickels = float(input("How many nickles?: "))
                 pennies = float(input("How many pennies?: "))
                 total = round((0.25 * quarters) + (0.10 * dimes) + (0.05 * nickels) + (0.01 * pennies), 2)
-                print(f"total coins inserted is ${total}")
+                print(f"total coins inserted is ${total}\n")
                 if total >= cappuccino_price:
                     change = round(total - cappuccino_price, 2)
                     print(f"Here is ${change} in change.")
                     print(f"Here is your {choice}. enjoy!")
                     resources["money"] = resources["money"] + cappuccino_price
-                    print(f"Current money in the machine = ${resources['money']}")
                     water = water - MENU["cappuccino"]["ingredients"]["water"]
+                    milk = milk - MENU["cappuccino"]["ingredients"]["milk"]
                     coffee = coffee - MENU["cappuccino"]["ingredients"]["coffee"]
-                    print(f"After this drink, machine holds: {water} water and: {coffee} coffee")
                 elif total < cappuccino_price:
-                    print("Sorry that's not enough money. Money refunded.")
+                    print("Sorry that's not enough money. Money refunded.\n")
 
         elif choice == "report":
-            print(f"{report(resources)}")
+            print(f"Water: {water}\n"
+                  f"Milk: {milk}\n"
+                  f"coffee: {coffee}\n"
+                  f"money: ${resources['money']}\n")
 
         elif choice == "off":
             machine_is_on = False
@@ -117,4 +121,4 @@ while machine_is_on:
             print("Goodbye!")
 
         else:
-            print("Please enter a valid input")
+            print("Please enter a valid input\n")
