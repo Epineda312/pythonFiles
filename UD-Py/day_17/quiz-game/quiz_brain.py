8b328371324a989dc7f77ced3a13fecd15f3870d
Airic <epineda773@protonmail.com>
2021-03-25 03:27:50 -0500

    Add files via upload

---
+++ UD-Py/day_17/quiz-game/quiz_brain.py
@@ -0,0 +1,27 @@
+# TODO 1: Asking the questions
+# TODO 2: Checking if the answer was correct
+# TODO 3: Checking if we're at the end of the quiz
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.questions_list = q_list
+        self.score = 0
+
+    def next_question(self):
+        current_question = self.questions_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("You got it wrong!")
+        print(f"the correct answer was {correct_answer}")
+        print(f"your current score is: {self.score}/{self.question_number}\n")
+
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)
