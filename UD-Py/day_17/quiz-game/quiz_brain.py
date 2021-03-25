f354d9d4f2f04d1e3be345eaeb57d9acf29a76d8
Airic <epineda773@protonmail.com>
2021-03-25 03:47:27 -0500

    Add files via upload
    changed data to retrieve questions provided by the Open Trivia DB API (I chose computer related questions, easy difficulty) instead.

--- UD-Py/day_17/quiz-game/quiz_brain.py
+++ UD-Py/day_17/quiz-game/quiz_brain.py
@@ -11,7 +11,7 @@ class QuizBrain:
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
