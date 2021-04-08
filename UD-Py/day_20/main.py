2681ae7bc5a6095ad32d6784e3904ed7a3a9d9be
Airic <epineda773@protonmail.com>
2021-04-08 01:04:07 -0500

    Add files via upload

--- UD-Py/day_20/main.py
+++ UD-Py/day_20/main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +24,9 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
-
-screen.exitonclick()
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
