4fbd8238d9ee76babbd2a171e6fcce8302eae5be
Airic <epineda773@protonmail.com>
2021-04-12 02:43:47 -0500

    Add files via upload
    Create second paddle. Move paddle code to paddle.py and create paddle class. Create ball class and add functionality so ball moves at a reasonable speed.

--- UD-Py/day_22/main.py
+++ UD-Py/day_22/main.py
@@ -1,30 +1,30 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+
 
 screen = Screen()
-screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.setup(width=800, height=600)
 screen.title("PONG")
+screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(350, new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(350, new_y)
-
+ball = Ball()
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
 
 screen.exitonclick()
