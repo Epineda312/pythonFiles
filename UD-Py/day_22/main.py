f599348527b54ddcc398315387707a1e2d4efb52
Airic <epineda773@protonmail.com>
2021-04-12 03:55:34 -0500

    Add files via upload
    Add functionality that keeps score. Change onkey to onkeypress for screen listeners to allow users to hold down the up and down movement keys for continuous movement.

--- UD-Py/day_22/main.py
+++ UD-Py/day_22/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,26 +12,36 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # Detect collision with right paddle.
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) and ball.xcor() < -320:
+    # Detect collision with right and left paddle.
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    # Detect if R paddle misses
+    if ball.xcor() > 370:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect if L paddle misses
+    if ball.xcor() < -370:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
